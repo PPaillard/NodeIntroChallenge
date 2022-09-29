34f366e455bbc7d3d1dc4b5f552684fe7c1d32cf
PierreP <paillard.pierre@gmail.com>
2022-09-29 11:44:00 +0200

    using Colors

---
+++ index.js
@@ -0,0 +1,6 @@
+import color from "colors";
+
+console.log(color.rainbow('Mohamed Amine'))
+console.log(color.bgBlue('Arnaud'))
+console.log(color.white('Peter'))
+console.log(color.yellow('Franck'))
